b1a3c2678e51f49ca8d50061202a96e261dbe5f2
broletsrust <rustrustrustrustrust@protonmail.com>
2023-02-24 17:59:01 +0100

    draw containers

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,30 @@
 use std::io;
 
-use crossterm::{terminal::{enable_raw_mode, EnterAlternateScreen, disable_raw_mode, LeaveAlternateScreen}, execute, event::{EnableMouseCapture, DisableMouseCapture}};
+use crossterm::{terminal::{enable_raw_mode, EnterAlternateScreen, disable_raw_mode, LeaveAlternateScreen}, execute, event::{EnableMouseCapture, DisableMouseCapture, Event, self, KeyCode}};
 use tui::{backend::CrosstermBackend, Terminal};
 
 mod game;
 
 fn main() -> Result<(), io::Error> {
+    // setup terminal
     enable_raw_mode()?;
     let mut stdout = io::stdout();
     execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
     let backend = CrosstermBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
 
+    loop {
+        if let Event::Key(key) = event::read()? {
+            match key.code {
+                KeyCode::Char('q') => {
+                    break;
+                }
+                _ => {}
+            }
+        }
+    }
+
+    // restore terminal
     disable_raw_mode()?;
     execute!(
         terminal.backend_mut(),
